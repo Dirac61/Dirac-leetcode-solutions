c5c9a394542dff57740251315b66fd085ce53887
Dirac <954646328@qq.com>
2026-07-27 15:10:29 +0800

    55

---
+++ c++/55-跳跃游戏.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i + nums[i]>max) {
+                max = i + nums[i];
+            }
+            if (i> max ) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
